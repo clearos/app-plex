6442b905966a666d2dc2fd1e86583292486e7e6b
pcbaldwin <pbaldwin@clearfoundation.com>
2014-09-03 14:21:00 -0400

    Creating ClearOS6 branch and tag

--- deploy/info.php
+++ deploy/info.php
@@ -4,7 +4,7 @@
 // General information
 ///////////////////////////////////////////////////////////////////////////// 
 $app['basename'] = 'plex';
-$app['version'] = '1.0.4';
+$app['version'] = '1.6.5';
 $app['release'] = '1';
 $app['vendor'] = 'eGloo';
 $app['packager'] = 'eGloo';
--- packaging/app-plex.spec
+++ packaging/app-plex.spec
@@ -1,7 +1,7 @@
 
 Name: app-plex
 Epoch: 1
-Version: 1.0.4
+Version: 1.6.5
 Release: 1%{dist}
 Summary: Plex Media Server
 License: GPLv3
